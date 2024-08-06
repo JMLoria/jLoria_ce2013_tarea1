2f4208b84935f0f725ca61ab622839bad8fdaf1a
JMLoria <joloria@estudiantec.cr>
2024-08-06 17:56:12 -0600

    Reviewing functionalities

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include "Generator.h"
+#include "PagedArray.h"
 
 int main() {
-    std::string fileName = "test1";
-    std::string size = "SMALL";
-
-    Generator generator(size, fileName);
-    std::cout << "Array generado y guardado en " << fileName << ".bin con " << generator.getArray().size() << " elementos." << std::endl;
-
-    return 0;
+//    std::string sizeFile = "SMALL";
+//    std::string pathFile = "~/Documents/TEC/II Semestre - 2024/CE2103 Datos II/Tareas/"
+//                           "CE2103-Tarea1-JoseManuelLoria/Test1";
+//
+//    std::cout << "Tarea Extraclase I - Arreglos Paginados \n";
+//    std::cout << "Estudiante: Jose Manuel Loria Cordero \n";
+//    std::cout << "Está ejecutando Generator.cpp \n";
+//    std::cout << "Argumentos ingresados:\n";
+//    std::cout << "\tSize:" << sizeFile << "\n";
+//    std::cout << "\tOutput path:" << pathFile << "\n";
+//
+//    Generator generator(sizeFile, pathFile);
+//
+//
+//    std::string inputFile = "~/Documents/TEC/II Semestre - 2024/CE2103 Datos II/Tareas/"
+//                            "CE2103-Tarea1-JoseManuelLoria/Test1";
+//    std::string outputFile = "x";
+//
+//    std::cout << "Tarea Extraclase I - Arreglos Paginados \n";
+//    std::cout << "Estudiante: Jose Manuel Loria Cordero \n";
+//    std::cout << "Está ejecutando PagedArray.cpp \n";
+//    std::cout << "Argumentos ingresados:\n";
+//    std::cout << "\tInput path:" << inputFile << "\n";
+//    std::cout << "\tOutput path:" << outputFile << "\n";
+//
+//    PagedArray array(inputFile);
+//    for (int i = 0; i < 500; i ++){
+//        std::cout << array[i * 32] << ", ";
+//        if((i + 1) % 20 == 0) {
+//            std::cout << "\n";
+//        }
+//    }
+//    array.printInfo();
 }
