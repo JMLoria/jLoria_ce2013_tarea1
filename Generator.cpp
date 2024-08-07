b9160e793e503166cf5c568295c511398ee26cc1
JMLoria <joloria@estudiantec.cr>
2024-08-06 21:28:38 -0600

    Final Commit

--- Generator.cpp
+++ Generator.cpp
@@ -1,6 +1,18 @@
 #include "Generator.h"
 #include <filesystem>
 
+/*
+ *
+░█████╗░██╗░░░░░░█████╗░░██████╗░██████╗░░░░░░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+██╔══██╗██║░░░░░██╔══██╗██╔════╝██╔════╝░░░░░██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░╚═╝██║░░░░░███████║╚█████╗░╚█████╗░░░░░░██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+██║░░██╗██║░░░░░██╔══██║░╚═══██╗░╚═══██╗░░░░░██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+╚█████╔╝███████╗██║░░██║██████╔╝██████╔╝░░░░░╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+░╚════╝░╚══════╝╚═╝░░╚═╝╚═════╝░╚═════╝░░░░░░░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+ */
+
+
+// 𝑪𝒐𝒏𝒔𝒕𝒓𝒖𝒄𝒕𝒐𝒓
 Generator::Generator(const std::string& sizeStr, const std::string& fileName){
     std::srand(std::time(nullptr));
 
@@ -10,53 +22,67 @@ Generator::Generator(const std::string& sizeStr, const std::string& fileName){
 }
 
 
+// 𝑮𝒆𝒏𝒆𝒓𝒂𝒕𝒆 𝑨𝒓𝒓𝒂𝒚
 void Generator::generateArray(Generator::SIZE size){
+    // Calcular el tamaño del array
     std::size_t numElements = size / sizeof(int);
-
+    // Alocar memoria para el array
     array.clear();
     array.reserve(numElements);
-
+    // Escribir valores aleatorios
     for (std::size_t i = 0; i < numElements; i++){
         array.push_back(std::rand());
     }
 }
 
 
+// 𝑮𝒆𝒕 𝑨𝒓𝒓𝒂𝒚
 const std::vector<int>& Generator::getArray() const {
+    // Retorna el array
     return array;
 }
 
 
+// 𝑷𝒓𝒊𝒏𝒕 𝑨𝒓𝒓𝒂𝒚
 void Generator::printArray() const {
+    // Imprime en consola el array
     for (std::size_t i = 0; i < array.size(); ++i) {
         std::cout << array[i] << ", ";
+        // Cada 10 valores, salta a la siguiente linea
         if ((i+1) % 10 == 0){
             std::cout << std::endl;
         }
     }
-
 }
 
 
+//𝑺𝒂𝒗𝒆 𝑭𝒊𝒍𝒆
 void Generator::saveFile(const std::string &fileName, SIZE size) const {
+    // Crea el path con el nombre del archivo
     std::filesystem::path filePath = fileName;
+    // Crea el archivo binario
     std::ofstream file(filePath, std::ios::binary);
-
+    // Valida que el archivo se haya creado o abierto
     if (!file) {
         throw std::runtime_error("No se pudo abrir el archivo para escritura");
     }
-
+    // Escribe el array en el archivo
     file.write(reinterpret_cast<const char*>(array.data()), array.size() * sizeof(int));
     file.close();
 }
 
 
+// 𝑷𝒂𝒓𝒔𝒆 𝑺𝒊𝒛𝒆
 Generator::SIZE Generator::parseSize(const std::string& sizeStr) {
+    // Establece el tamaño que va a tener el archivo segun el string ingresado
     if (sizeStr == "SMALL") {
+        // Establece el tamaño pequeño
         return SMALL;
     } else if (sizeStr == "MEDIUM") {
+        // Establece el tamaño medio
         return MEDIUM;
     } else if (sizeStr == "LARGE") {
+        // Establece el tamaño grande
         return LARGE;
     } else{
         throw std::invalid_argument("Tamaño no válido. Pruebe: 'SMALL', 'MEDIUM', 'LARGE'");
@@ -64,6 +90,7 @@ Generator::SIZE Generator::parseSize(const std::string& sizeStr) {
 }
 
 
+// 𝑬𝒙𝒆𝒄𝒖𝒕𝒆
 int main(int argc, char *argv[]){
     std::string sizeFile = argv[2];
     std::string pathFile = argv[4];
--- PagedArray.cpp
+++ PagedArray.cpp
@@ -1,12 +1,24 @@
 #include "PagedArray.h"
 
+/*
+ *
+░█████╗░██╗░░░░░░█████╗░░██████╗░██████╗░░░░░██████╗░░█████╗░░██████╗░███████╗██████╗░░█████╗░██████╗░██████╗░░█████╗░██╗░░░██╗
+██╔══██╗██║░░░░░██╔══██╗██╔════╝██╔════╝░░░░░██╔══██╗██╔══██╗██╔════╝░██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗░██╔╝
+██║░░╚═╝██║░░░░░███████║╚█████╗░╚█████╗░░░░░░██████╔╝███████║██║░░██╗░█████╗░░██║░░██║███████║██████╔╝██████╔╝███████║░╚████╔╝░
+██║░░██╗██║░░░░░██╔══██║░╚═══██╗░╚═══██╗░░░░░██╔═══╝░██╔══██║██║░░╚██╗██╔══╝░░██║░░██║██╔══██║██╔══██╗██╔══██╗██╔══██║░░╚██╔╝░░
+╚█████╔╝███████╗██║░░██║██████╔╝██████╔╝░░░░░██║░░░░░██║░░██║╚██████╔╝███████╗██████╔╝██║░░██║██║░░██║██║░░██║██║░░██║░░░██║░░░
+░╚════╝░╚══════╝╚═╝░░╚═╝╚═════╝░╚═════╝░░░░░░╚═╝░░░░░╚═╝░░╚═╝░╚═════╝░╚══════╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░
+ */
 
+
+// 𝑪𝒐𝒏𝒔𝒕𝒓𝒖𝒄𝒕𝒐𝒓
 PagedArray::PagedArray(const std::string &memDir) {
+    // Inicializar el documento binario (memoria)
     memory.open(memDir, std::ios::in | std::ios::out);
-
+    // Inicializar pageHit y pageFault
     pageHit = 0;
     pageFault = 0;
-
+     // Inicializar los frames del paged array
     frame1 = Page();
     frame2 = Page();
     frame3 = Page();
@@ -14,20 +26,25 @@ PagedArray::PagedArray(const std::string &memDir) {
 }
 
 
+// 𝑶𝒑𝒆𝒓𝒂𝒅𝒐𝒓 []
 int PagedArray::operator[](int index) {
+    // Calcular el indice relativo y el numero de pagina
     int relativeIndex = index % 128;
     int pageNum = index / 128;
-
+    // Generar un ID al frame que queremos cargar a partir de los frames cargados
     int frameID = frameLoaded(pageNum);
-
+    // Validar si es un frame cargado
     if (frameID < 0) {
+        // Cargar el nuevo frame
         pageFault ++;
         int frameNum = checkLoadedFrame();
         loadFrame(frameNum, pageNum);
     } else {
+        // Frame cargado anteriormente
         pageHit ++;
     }
 
+    // Retornar numero del frame cargado
     if(frame1.getNumPage() == pageNum) {
         return frame1[relativeIndex];
     } else if(frame2.getNumPage() == pageNum) {
@@ -41,21 +58,29 @@ int PagedArray::operator[](int index) {
 }
 
 
+// 𝑪𝒉𝒆𝒄𝒌 𝑳𝒐𝒂𝒅𝒆𝒅 𝑭𝒓𝒂𝒎𝒆
 int PagedArray::checkLoadedFrame() {
+    // Verificar cuales frames estan ocupados y cuales estan vacios
     if(!frame1.getLoaded()) {
+        // Frame 1 vacio
         return 1;
     } else if(!frame2.getLoaded()) {
+        // Frame 2 vacio
         return 2;
     } else if(!frame3.getLoaded()) {
+        // Frame 3 vacio
         return 3;
     } else if(!frame4.getLoaded()) {
+        // Frame 4 vacio
         return 4;
     } else {
+        // Paged Arrey lleno
         return -1;
     }
 }
 
 
+// 𝑭𝒓𝒂𝒎𝒆 𝑳𝒐𝒂𝒅𝒆𝒅
 int PagedArray::frameLoaded(int pageNum) {
     if(frame1.getNumPage() == pageNum) {
         return 1;
@@ -71,30 +96,37 @@ int PagedArray::frameLoaded(int pageNum) {
 }
 
 
+// 𝑳𝒐𝒂𝒅 𝑭𝒓𝒂𝒎𝒆
 void PagedArray::loadFrame(int frameNum, int pageNum) {
+    // Cargar nuevo frame al paged array
     int tempFrame [128];
+    // Extraer del documento el frame que se va a cargar
     memory.seekg(pageNum * sizeof(int) * 128);
     memory.read((char*) &tempFrame, 128 * sizeof(int));
-
+    // Definir a cual page se va a cargar el frame
     switch(frameNum){
         case 1:
-            // loadMemory
+            // Cargar a frame 1
             frame1 = Page(tempFrame, pageNum);
             break;
 
         case 2:
+            // Cargar a frame 2
             frame2 = Page(tempFrame, pageNum);
             break;
 
         case 3:
+            // Cargar a frame 3
             frame3 = Page(tempFrame, pageNum);
             break;
 
         case 4:
+            // Cargar a frame 4
             frame4 = Page(tempFrame, pageNum);
             break;
 
         default:
+            // Paged array lleno, seleccionar frame al que se va a sobreescribir
             int randFrame = 1 + rand() % 4;
             loadFrame(randFrame, pageNum);
             break;
@@ -102,11 +134,13 @@ void PagedArray::loadFrame(int frameNum, int pageNum) {
 }
 
 
+// 𝑷𝒓𝒊𝒏𝒕 𝑰𝒏𝒇𝒐
 void PagedArray::printInfo() {
     std::cout << "Page hit = " << pageHit << std::endl <<  "Page fault = " << pageFault << std::endl;
 }
 
 
+// 𝑬𝒙𝒆𝒄𝒖𝒕𝒆
 int main(int argc, char *argv[]){
     std::string inputFile = argv[2];
     std::string outputFile = argv[4];
